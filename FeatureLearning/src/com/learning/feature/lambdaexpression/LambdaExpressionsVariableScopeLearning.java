e28a68dc4cea1592278856f2d8b566b9f3796518
Ariboyina <PRASAD_ARIBOYINA@homedepot.com>
2018-05-30 15:59:25 +0530

    Lambda examples from official documentation

--- FeatureLearning/src/com/learning/feature/lambdaexpression/LambdaExpressionsVariableScopeLearning.java
+++ FeatureLearning/src/com/learning/feature/lambdaexpression/LambdaExpressionsVariableScopeLearning.java
@@ -8,11 +8,13 @@ public class LambdaExpressionsVariableScopeLearning {
 	public static void main(String[] args)
 	{
 		LambdaExpressionsVariableScopeLearning st = new LambdaExpressionsVariableScopeLearning();
-		LambdaExpressionsVariableScopeLearning.FirstLevel fl = st.new FirstLevel();
+		LambdaExpressionsVariableScopeLearning.FirstLevelLambda flambda = st.new FirstLevelLambda();
+        flambda.methodInFirstLevel(23);
+        LambdaExpressionsVariableScopeLearning.FirstLevel fl = st.new FirstLevel();
         fl.methodInFirstLevel(23);
 	}
 	
-	class FirstLevel {
+	class FirstLevelLambda {
 		public int x = 2;
 		
 		void methodInFirstLevel(int x) 
@@ -27,4 +29,17 @@ public class LambdaExpressionsVariableScopeLearning {
 			myConsumer.accept(x);
 		}
 	}
+	
+	class FirstLevel {
+		public int x = 0;
+		
+		void methodInFirstLevel(int x) 
+		{
+			System.out.println("x = " + x); // 23
+			System.out.println("this.x = " + this.x); // 0
+			System.out.println("LambdaExpressionsVariableScopeLearning.this.x = " +
+					LambdaExpressionsVariableScopeLearning.this.x); //1
+		}
+	}
+	
 }
